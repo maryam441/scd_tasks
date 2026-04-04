ee70e2d083dae0e48c6e2c5b3c80f79e7f9d777f
maryam441 <maryam441@users.noreply.github.com>
2026-04-05 00:00:07 +0500

    Complete project upload

---
+++ week3/src/components/Section4.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Section4() {
+  return (
+    <div className="bg-[#050522] px-6 md:px-20 py-20 ">
+        <div className="max-w-6xl mx-auto">
+          <h2 className="text-3xl font-bold mb-6">Services I Offer</h2>
+
+          <p className="text-white text-sm max-w-f mb-16">
+            Legendary assassin John Wick (Keanu Reeves) retired from his violent career after
+            marrying the love of his life. Her sudden death leaves John in deep mourning.
+            When sadistic mobster Iosef Tarasov (Alfie Allen) and his thugs steal Johns prized
+            car and kill the puppy that was a last gift from his wife, John unleashes the
+            remorseless killing machine within and seeks vengeance.
+          </p>
+        </div>
+      </div>
+  )
+}
